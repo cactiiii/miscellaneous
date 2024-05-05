343b0d181aa220a010bbf21518d7f691995fc182
kai <cyokel@163.com>
2024-05-05 18:16:41 +0800

    modify

--- ocserv/certificates/gen.sh
+++ ocserv/certificates/gen.sh
@@ -4,4 +4,3 @@ certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --o
 certtool --generate-privkey --outfile server-key.pem
 certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
 
-echo then you can copy server-cert.pem server-key.pem to /etc/ocserv/
