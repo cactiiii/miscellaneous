bf2d4865cbcf7ae684daf8338c2b5cb3ebcdbfa8
Your Name <you@example.com>
2025-12-06 22:54:58 +0800

    update

--- ocserv/certificates/gen.sh
+++ ocserv/certificates/gen.sh
@@ -3,4 +3,6 @@ certtool --generate-privkey --outfile ca-key.pem
 certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile  ca-cert.pem
 certtool --generate-privkey --outfile server-key.pem
 certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
+certtool --generate-privkey --outfile user-key.pem
+certtool --generate-certificate --load-privkey user-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template user.tmpl --outfile user-cert.pem
 
