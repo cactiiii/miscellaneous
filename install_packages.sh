d78b8c4f0d913e3ed79c1d31fe291217238ce977
Your Name <you@example.com>
2026-03-07 13:14:12 +0800

    add sh

---
+++ install_packages.sh
@@ -0,0 +1,18 @@
+yum install git
+yum install gcc-c++
+yum install nettle-devel
+yum install gnutls-devel
+yum install libev-devel
+yum install readline-devel
+yum install lz4-devel
+yum install gnutls-utils
+systemctl stop firewalld
+systemctl disable firewalld
+yum install iptables-services
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.conf
+
+yum install nodejs
+yum install npm
+npm install hexo
+npm install hexo-pdf
