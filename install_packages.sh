e1dfcaf81c065821ba16af92ec38c7c79bb8a3cc
ckai <cyokel@163.com>
2026-03-08 22:24:16 +0800

    up

--- install_packages.sh
+++ install_packages.sh
@@ -13,6 +13,7 @@ systemctl disable firewalld
 yum install iptables-services
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.conf
+systemctl enable iptables
 
 yum install nodejs
 yum install npm
@@ -22,6 +23,20 @@ npm install hexo-pdf
 mkdir -p ~/downloads
 cd ~/downloads
 wget https://www.infradead.org/ocserv/download/ocserv-1.4.0.tar.xz
+tar -xvf ocserv-1.4.0.tar.xz
+cp ~/downloads/ocserv-1.4.0/doc/systemd/standalone/ocserv.service /usr/lib/systemd/system/
+systemctl enable ocserv
 cd -
 
 adduser -U -m ocserv
+
+# 设置iptables
+DIR=`dirname $0`
+if [ -f /etc/sysconfig/iptables ]; then
+    mv /etc/sysconfig/iptables /etc/sysconfig/iptables-bak
+fi
+echo DIR is $DIR
+cp $DIR/ocserv/iptables /etc/sysconfig/iptables
+systemctl restart iptables
+
+yum install nginx
